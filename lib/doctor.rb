0f7d7b149f64322d41ba7d1c8aa3f3cddd41ec17
Brian Grant and Bang Nguyen <brian@>
2016-07-12 09:24:39 -0700

    add speciality param to Doctor class

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,15 +1,16 @@
 class Doctor
-  attr_reader(:name, :id)
+  attr_reader(:name, :id, :specialty)
 
   @@doctors = []
 
   define_method (:initialize) do |attributes|
     @name = attributes.fetch(:name)
     @id = @@doctors.length.+(1)
+    @specialty = attributes.fetch(:specialty)
   end
 
   define_singleton_method (:all) do
-    @@doctors 
+    @@doctors
   end
 
   define_method (:save) do
@@ -20,7 +21,13 @@ class Doctor
     @@doctors = []
   end
 
-  # define_method (:id) do
-  #
-  # end
+  define_singleton_method (:find) do |id|
+    doctor_found = nil
+    @@doctors.each do |doctor|
+      if doctor.id.eql?(id)
+        doctor_found = doctor.name()
+      end
+    end
+    doctor_found
+  end
 end
--- spec/doctor_spec.rb
+++ spec/doctor_spec.rb
@@ -4,7 +4,7 @@ require ('doctor')
 describe ('Doctor') do
   describe ('#name') do
     it('will create new doctor') do
-      test_doctor = Doctor.new({:name => 'Patch Adams'})
+      test_doctor = Doctor.new({:name => 'Patch Adams', :specialty => "Dental"})
       expect(test_doctor.name()).to(eq('Patch Adams'))
     end
   end
@@ -17,16 +17,16 @@ describe ('Doctor') do
 
   describe('#save') do
     it('saves the doctor to the array of saved doctors') do
-      test_doctor = Doctor.new({:name => "Patch Adams"})
+      test_doctor = Doctor.new({:name => "Patch Adams", :specialty => "Dental"})
       expect(test_doctor.save()).to(eq([test_doctor]))
     end
   end
 
   describe('.clear') do
     it('clears the array of saved doctors') do
-      test_doctor = Doctor.new({:name => "Patch Adams"})
+      test_doctor = Doctor.new({:name => "Patch Adams", :specialty => "Dental"})
       test_doctor.save()
-      test_doctor2 = Doctor.new({:name => "George Washington Carver"})
+      test_doctor2 = Doctor.new({:name => "George Washington Carver", :specialty => "Eyes"})
       test_doctor2.save()
       Doctor.clear()
       expect(Doctor.all()).to(eq([]))
@@ -35,8 +35,16 @@ describe ('Doctor') do
 
   describe('#id') do
     it('returns ID of a doctor in the office') do
-      test_doctor = Doctor.new({:name => "Patch Adams"})
+      test_doctor = Doctor.new({:name => "Patch Adams", :specialty => "Dental"})
       expect(test_doctor.id()).to(eq(1))
+    end
+  end
+
+  describe('.find') do
+    it('find the doctor by his/her ID') do
+      test_doctor = Doctor.new({:name => "Patch Adams", :specialty => "Dental"})
+      test_doctor.save()
+      expect(Doctor.find(1)).to(eq("Patch Adams"))
     end
   end
 
